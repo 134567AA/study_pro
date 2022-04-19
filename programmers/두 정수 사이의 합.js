76c868a0ea029a07dc43232b53e82765c586f507
134567AA <fnqlqhdms@naver.com>
2022-04-19 17:46:14 +0900

    0419 화요일

---
+++ programmers/두 정수 사이의 합.js
@@ -0,0 +1,9 @@
+
+function solution(a,b){
+    let sum = 0;
+    for(let i=a; i<b+1; i++){
+        sum += i;
+    }
+    return sum;
+}
+solution(a,b);
---
+++ programmers/약수 사이의 합.js
@@ -0,0 +1,8 @@
+function solution(n){
+    let sum =0;
+    for(let i=0; i <= n ; i++ ){
+        n%i===0 ? sum += i  : "띵" 
+    }
+    return sum;
+}
+console.log(solution(12));
