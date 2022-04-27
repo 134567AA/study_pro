62d75546a15889f3bfe0abf97d50c558cf38f563
134567AA <fnqlqhdms@naver.com>
2022-04-27 18:43:30 +0900

    0427 sort 함수

--- programmers/나누어 떨어지는 숫자 배열.js
+++ programmers/나누어 떨어지는 숫자 배열.js
@@ -16,3 +16,13 @@ function solution(arr,divisor) {
    
 }
 solution(arr,divisor);
+
+// 오름차순
+const aa = [10, 3, 12, 2];
+
+const b = aa.sort((a, b)=>{ return a - b });
+console.log(`오름차순 : ${b}`);
+
+// 내림차순
+const c = aa.sort((a, b)=>{ return b - a });
+console.log(`내림차순 : ${c}`);
