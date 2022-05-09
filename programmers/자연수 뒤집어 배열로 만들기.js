c15e1612f33826282524b52bc4d7164f8abad2a7
134567AA <fnqlqhdms@naver.com>
2022-05-09 17:45:32 +0900

    0509

---
+++ programmers/자연수 뒤집어 배열로 만들기.js
@@ -0,0 +1,14 @@
+ const a = 12345;
+ 
+
+ const solution = (n) =>{
+    const arr = []; // 빈 배열 만들어줌
+    const b = String(a).split('').reverse(); // number >> string : String() | string >> parseInt() 
+   
+     for(let i=0; i < b.length; i++){  //["5","4","3","2","1"] 문자타입임 
+     arr.push(parseInt(b[i])); //빈 배열 arr에 문자타입을 숫자로 바꿔서 푸쉬
+    }
+     return arr;
+}
+
+console.log(solution(a));
