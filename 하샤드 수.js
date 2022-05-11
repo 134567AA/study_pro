c216ee818985da7eea830ecbcecd8914c8677a34
134567AA <fnqlqhdms@naver.com>
2022-05-11 17:03:06 +0900

    0511

--- 하샤드 수.js
+++ 하샤드 수.js
@@ -1,6 +1,19 @@
-const a = 10;
+const a = 11;
 
-function solution(x) {
-    String(a);
+function solution(arr) {
+    let sum =0;
+    const b =(String(arr).split(""));
+    console.log(b)
+
+
+    for(let i = 0 ; i < b.length; i++){
+        sum += parseInt(b[i]);
+     
+    }   
+    console.log(sum);
+    if(a % sum === 0){
+    return true;
+    }else return false
+  
 }
-solution(a);
+console.log(solution(a));
